02c4f7f1b6666f57ca99e921c3c30311d5571648
Amit Pundir <amit.pundir@linaro.org>
2012-11-21 15:30:45 +0530

    Android.mk: modify TARGET_BOARD if condition to support Origen 4412
    The corresponding CFLAG is valid for both variants of Origen so we
    need to modify this if condition.
    
    Change-Id: I1164a613dbebbc2183d772a5bf3aa79000c32a48

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ifeq ($(TARGET_BOARD_PLATFORM),origen)
+ifneq (,$(filter $(TARGET_PRODUCT),origen origen_quad))
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
